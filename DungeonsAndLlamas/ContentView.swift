c7d4bcfa712a0182f41ada70ef4afb558578211d
Robert Waltham <rob@blockoftext.com>
2024-03-26 16:29:39 -0700

    coordinator pattern

--- DungeonsAndLlamas/ContentView.swift
+++ DungeonsAndLlamas/ContentView.swift
@@ -7,55 +7,106 @@
 
 import SwiftUI
 import SwiftData
+import Observation
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State var path = NavigationPath()
+    @State var viewModel = ContentViewModel()
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ContentFlowCoordinator(flowState: viewModel) {
+            VStack {
+                NavigationLink("Link", value: ContentLink.firstLink(text: "text"))
+                Button("Sheet") {
+                    viewModel.sheet()
+                }.buttonStyle(.bordered)
+                Button("Cover") {
+                    viewModel.cover()
+                }.buttonStyle(.bordered)
+                
+                GeometryReader { proxy in
+                    Button("Popover") {
+                        viewModel.popover(bounds: .rect(.rect(proxy.frame(in: .global))))
                     }
+                    .buttonStyle(.bordered)
+                    .position(x: proxy.frame(in: .local).width / 2.0, y: proxy.frame(in: .local).height / 2.0)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+                .frame(maxWidth: 200, maxHeight: 100)
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+                
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
 
+struct ContentFlowCoordinator<Content: View>: View {
+    @State var flowState: ContentViewModel
+    let content: () -> Content
+
+    var body: some View {
+        NavigationStack(path: $flowState.path) {
+            ZStack {
+                content()
+                    .sheet(item: $flowState.presentedItem, content: sheetContent)
+                    .fullScreenCover(item: $flowState.coverItem, content: coverContent)
+            }
+            .navigationDestination(for: ContentLink.self, destination: linkDestination)
+            .popover(item: $flowState.popoverItem, attachmentAnchor: flowState.popoverBounds, content: popoverContent)
+        }
+    }
+}
+
+extension ContentFlowCoordinator {
+    @ViewBuilder private func linkDestination(link: ContentLink) -> some View {
+        Text("Link Destination \(link.id)")
+    }
+    
+    @ViewBuilder private func sheetContent(link: ContentLink) -> some View {
+        Text("Sheet Content \(link.id)")
+    }
+    
+    @ViewBuilder private func coverContent(link: ContentLink) -> some View {
+        Text("Cover content \(link.id)")
+    }
+    
+    @ViewBuilder private func popoverContent(link: ContentLink) -> some View {
+        Text("Popover content \(link.id)")
+    }
+}
+
+@Observable
+class ContentFlowState {
+    var path = NavigationPath()
+    var presentedItem: ContentLink?
+    var coverItem: ContentLink?
+    var popoverItem: ContentLink?
+    var popoverBounds: PopoverAttachmentAnchor = .rect(.rect(CGRect()))
+}
+
+class ContentViewModel: ContentFlowState {
+    func sheet() {
+        presentedItem = .firstLink(text: "first link")
+    }
+    
+    func cover() {
+        coverItem = .secondLink(text: "second link")
+    }
+    
+    func popover(bounds: PopoverAttachmentAnchor) {
+        self.popoverBounds = bounds
+        popoverItem = .secondLink(text: "second link")
+    }
+}
+
+enum ContentLink: Identifiable, Hashable {
+    var id: String {
+        String(describing: self)
+    }
+    
+    case firstLink(text: String)
+    case secondLink(text: String)
+}
+
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
